a8f4b7b4296d44b7d0d487717d6e1a61b1199e0a
Lin Xue <lx58@cornell.edu>
2016-12-12 13:31:19 -0500

    final edit, all work.

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,70 @@ function bindEvent(el, eventName, eventHandler) {
 		el.attachEvent('on'+eventName, eventHandler);
 	}
 }
-
+function downloadWithBlob(e) {
+		//Don't submit form yet
+		e.preventDefault();
+		var email = $("#email").val();
+		var canvas1 = document.getElementById('canvas').fabric;
+		var baseImg = canvas1.item(0);
+		//check if base image exist
+		if(!baseImg) {
+			$("#result").text('Please take a selfie or upload a photo first!').css("color", "red");
+		}
+		//validate email first
+		else if (validateEmail(email)) {
+			//Clear error msg
+			$("#result").text('');
+			/*//change the hidden sticker's css class to regular sticker  //re-bind click event to hidden sticker
+			$("#hidden_sticker").removeClass("hidden_sticker").addClass("picedit_sticker");
+			$("#sticker_lock").hide();
+			// To re-enable click event
+			document.getElementById('hidden_sticker').style.pointerEvents = 'auto';*/
+			//prepare zipfile
+			var zip = new JSZip();
+			//Remove existing sticker if any
+			var exSticker = canvas1.item(1);
+			if (exSticker) {
+				canvas1.remove(exSticker);
+			}
+			// Get all img elements
+			var stickers = document.getElementsByTagName('img');
+			for (var i=0; i<stickers.length; i++) {
+				var stickerID = stickers[i].id;
+				/*if (sticker) {
+					canvas1.remove(sticker);
+				}*/
+				var imgElement = document.getElementById(stickerID);
+				var sticker = new fabric.Image(imgElement, {
+					left: 20,
+					top: 270,
+					scaleX: 1.5,
+					scaleY: 1.5,
+					lockUniScaling: true,
+					hasControls: false,
+					hasBorders: false
+				});
+				//Add new sticker to canvas
+				canvas1.insertAt(sticker,1, false);
+				var savable = new Image();
+				savable.src = canvas1.toDataURL();
+				var imageData = savable.src.substr(savable.src.indexOf(',')+1);
+				zip.file("profile_"+ i +".png", imageData, {base64: true});
+				canvas1.remove(sticker);
+			}
+			zip.generateAsync({type:"blob"}).then(function (blob) {
+				saveAs(blob, "CUprofile.zip");
+			}, function (err) {
+				blobLink.innerHTML += " " + err;
+			});
+			//now submit the form
+			$("#email_form").submit(); 
+		}
+		else {
+			$("#result").text("You must enter a valid email address!").css("color", "red");
+		}
+		return false;
+	}
 
 // main()
 $(function() {
@@ -37,93 +100,11 @@ $(function() {
     $('#thebox').picEdit();
     // prevent click event on hidden_sticker at first
     // document.getElementById('hidden_sticker').style.pointerEvents = 'none';
-/*    //popup form to collect user email
-    $('#downloadZip').on('click', function() {
-        if($(this).hasClass('selected')) {
-            deselect($(this));
-        } else {
-            $(this).addClass('selected');
-            $('.pop').slideFadeToggle();
-        }
-        return false;
-    });
-
-    $('.close').on('click', function() {
-        deselect($('#downloadZip'));
-        $("#result").text('');
-        return false;
-    });*/
-
     // Blob
     var blobLink = document.getElementById('message_submit');
     if (JSZip.support.blob) {
-        function downloadWithBlob(e) {
-            //Don't submit form yet
-            e.preventDefault();
-            var email = $("#email").val();
-            var canvas1 = document.getElementById('canvas').fabric;
-            var baseImg = canvas1.item(0);
-            //check if base image exist
-            if(!baseImg) {
-                $("#result").text('Please take a selfie or upload a photo first!').css("color", "red");
-            }
-            //validate email first
-            else if (validateEmail(email)) {
-                //Clear error msg
-                $("#result").text('');
-                /*//change the hidden sticker's css class to regular sticker  //re-bind click event to hidden sticker
-                $("#hidden_sticker").removeClass("hidden_sticker").addClass("picedit_sticker");
-                $("#sticker_lock").hide();
-                // To re-enable click event
-                document.getElementById('hidden_sticker').style.pointerEvents = 'auto';*/
-                //prepare zipfile
-                var zip = new JSZip();
-                //Remove existing sticker if any
-                var exSticker = canvas1.item(1);
-                if (exSticker) {
-                    canvas1.remove(exSticker);
-                }
-                // Get all img elements
-                var stickers = document.getElementsByTagName('img');
-                for (var i=0; i<stickers.length; i++) {
-                    var stickerID = stickers[i].id;
-                    /*if (sticker) {
-                        canvas1.remove(sticker);
-                    }*/
-                    var imgElement = document.getElementById(stickerID);
-                    var sticker = new fabric.Image(imgElement, {
-                        left: 20,
-                        top: 270,
-                        scaleX: 1.5,
-                        scaleY: 1.5,
-                        lockUniScaling: true,
-                        hasControls: false,
-                        hasBorders: false
-                    });
-                    //Add new sticker to canvas
-                    canvas1.insertAt(sticker,1, false);
-                    var savable = new Image();
-                    savable.src = canvas1.toDataURL();
-                    var imageData = savable.src.substr(savable.src.indexOf(',')+1);
-                    zip.file("profile_"+ i +".png", imageData, {base64: true});
-                    canvas1.remove(sticker);
-                }
-                zip.generateAsync({type:"blob"}).then(function (blob) {
-                    saveAs(blob, "CUprofile.zip");
-                }, function (err) {
-                    blobLink.innerHTML += " " + err;
-                });
-
-				//now submit the form
-				$("#email_form").submit(); 
-            }
-            else {
-                $("#result").text("You must enter a valid email address!").css("color", "red");
-            }
-            return false;
-        }
         bindEvent(blobLink, 'click', downloadWithBlob);
-    } else {
+    } else {	
         blobLink.innerHTML += " (not supported on this browser)";
     }
 
