4bd0fb040c8bccefc81ca2246c9d4f8b5d3da676
Lin Xue <lx58@cornell.edu>
2017-09-19 11:47:28 -0400

    start pack for single image editor

---
+++ src/js/image_main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lx58 on 10/25/2016.
+ */
+/*
+ *  Project: PhotoSticker plugin based on PicEdit by Andy V. and fabric.js
+ *  Description: Allows user to take a selfie or upload a photo with tools to edit the image on the front-end and apply stickers to it
+ *               The user can then download the profile, download a Zip file for all stickers and share on social media.
+ *  Author: Lin Xue
+ *  License: MIT
+ */
+
+
+// main()
+$(function() {
+    // Document is ready
+    // Start the plugin
+    $('.picedit_box').picEdit( );
+
+});
+//Lin: form
+$.fn.slideFadeToggle = function(easing, callback) {
+    return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+};
